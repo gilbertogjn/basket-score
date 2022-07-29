cb99dc4bc4abc1e95dcd28a172c990487f26c258
Gilberto Garcia <gilbertogjn@gmail.com>
2022-07-29 16:28:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let homeCount = 0
+let guestCount = 0
+
+function resetScore() {
+  homeCount = 0
+  guestCount = 0
+  homeScore.textContent = homeCount
+  guestScore.textContent = guestCount
+}
+
+function add1Home() {
+  homeCount += 1
+  homeScore.textContent = homeCount
+}
+
+function add2Home() {
+  homeCount += 2
+  homeScore.textContent = homeCount
+}
+
+function add3Home() {
+  homeCount += 3
+  homeScore.textContent = homeCount
+}
+
+function add1Guest() {
+  guestCount += 1
+  guestScore.textContent = guestCount
+}
+
+function add2Guest() {
+  guestCount += 2
+  guestScore.textContent = guestCount
+}
+
+function add3Guest() {
+  guestCount += 3
+  guestScore.textContent = guestCount
+}
